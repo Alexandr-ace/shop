7159bde5ddba3c74221f7341b761f5d82ba551c5
Alexandr-ace <syakubin@bk.ru>
2023-08-10 00:21:34 +0300

    modified:   package-lock.json

---
+++ src/components/List/List.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import One from "../One/One";
+import Two from "../Two/Two";
+
+const List = () => {
+  return (
+    <div className="List">
+      <One />
+      <Two />
+    </div>
+  );
+};
+
+export default List;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,26 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./components/ErrorPage/ErrorPage";
+import List from "./components/List/List";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "list",
+        element: <List />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <App />
+    <RouterProvider router={router} />
   </Provider>
 );
